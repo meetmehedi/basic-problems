2a7c29858a2ebc7a582f35aae39b95a2e2583a9d
MD. Mehedi Hasan <167318121+meetmehedi@users.noreply.github.com>
2024-11-04 22:32:06 +0600

    sum of the first N natural numbers.

---
+++ Problem02.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Problem02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int sum, i;
+        int n = input.nextInt();
+            sum = (n*(n + 1) / 2);
+            System.out.println(sum + " ");
+        }
+    }
